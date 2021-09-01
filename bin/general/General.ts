83d7c0193ff41c56d1851296d2cf22150ede6410
XfaonAE <xfaonae@gmail.com>
2021-09-01 14:22:52 -0400

    Chore: Added single run on dev server Todo: Add auto restart and cli input line

--- bin/general/General.ts
+++ bin/general/General.ts
@@ -2,9 +2,7 @@ import { appDevServer } from "../../src/Main";
 
 export default class General {
     public dev(args: any[], flags: any) {
-        const devServer = appDevServer.create({
-
-        });
+        const devServer = appDevServer.create();
 
         devServer.run();
     }
--- src/appDevServer/AppDevServer.ts
+++ src/appDevServer/AppDevServer.ts
@@ -1,3 +1,38 @@
+import { spawn } from "child_process";
+import { terminal, utils } from "../Main";
+import path from "path";
+
+export interface DevServerOptions {
+    main?: string;
+    root?: string;
+}
+
 export default class AppDevServer {
-    
+    public config!: DevServerOptions;
+
+    public constructor(options?: DevServerOptions) {
+        if (options) {
+            const config: DevServerOptions = utils.parseConfig({
+                main: "./src/Main",
+                root: process.cwd()
+            } as DevServerOptions, options);
+
+            this.config = config;
+        }
+    }
+
+    public create(options: DevServerOptions = {}) {
+        return new AppDevServer(options);
+    }
+
+    public run() {
+        terminal.header("Ember General App Dev Server");
+
+        const config = this.config;
+        const mainScript = spawn("node", [ path.join(config.root!, config.main!) ]);
+
+        mainScript.stdout.on("data", (data: Buffer) => {
+            process.stdout.write(data.toString());
+        });
+    }
 }
