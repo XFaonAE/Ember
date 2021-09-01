a481a667c9adcb45b64430adb49acc2c22505f7b
XfaonAE <xfaonae@gmail.com>
2021-09-01 17:34:17 -0400

    Chore: Added shims to template

---
+++ examples/tickTacToe/server.ts
@@ -0,0 +1,25 @@
+import { socket, terminal } from "../../src/Main";
+import ServerConnection from "../../src/socket/server/ServerConnection";
+
+const api = socket.createServer({
+    port: 1414,
+    host: "localhost",
+    nodes: [ {
+        port: 1414,
+        host: "localhost"
+    } ]
+});
+
+api.on("run", () => {
+    terminal.log("Server is starting");
+});
+
+api.on("ready", () => {
+    terminal.log("Server is ready");
+});
+
+api.on("open", (connection: ServerConnection) => {
+    connection.accept();
+});
+
+api.run();
