10bce1f684bb157b443d4c006d8b31996d0e5cf1
XfaonAE <xfaonae@gmail.com>
2021-09-02 22:08:33 -0400

    Chore: Removed bad query builder in exchange for an SQL query design

--- src/sql/Query.ts
+++ src/sql/Query.ts
@@ -1,184 +1,47 @@
-import mySql, { Connection } from "mysql";
-import { utils } from "../Main";
+import { Connection } from "mysql"
 
-export interface QueryRestrict {
-    limit?: number | null;
-}
-
-export interface InsertQuery {
-    table: string;
-    data: [{
-        column: string;
-        value: string;
-    }, ...{
-        column: string;
-        value: string;
-    }[]];
-    restrict?: QueryRestrict;
-}
-
-export interface SelectQuery {
-    table: string;
-    columns: [ string, string, ...string[] ] | string;
-    restrict?: QueryRestrict;
-}
+export interface MetaData {}
 
 export default class Query {
-    public connection: Connection;
-    public ready = false;
-    public restrictDefaults: QueryRestrict = {
-        limit: null
-    };
+    public connection: Connection
+    public ready = false
 
     public constructor(connection: Connection) {
-        this.connection = connection;
+        this.connection = connection
     }
 
     public readySet() {
         if (!this.ready) {
-            this.ready = true;
+            this.ready = true
         }
     }
 
-    public parsePlaceholders(group: string[], placeholder: "?" | "??" = "??"): string {
-        let placeholders = "";
-        let placeholderArray: string[] = [];
-
-        if (group.length == 1) {
-            return placeholder;
-        } else if (group.length > 1) {
-            group.forEach(() => placeholderArray.push(placeholder));
-        }
-
-        placeholders = placeholderArray.join(", ");
-        return placeholders;
-    }
-
-    public cleanResult(result: any): any[] {
-        const newResult: any[] = [];
-
-        result.forEach((record: any) => {
-            newResult.push({ ...record });
-        });
-
-        return newResult;
-    }
-
-    public insert(options: InsertQuery) {
-        this.isReady((error: any) => {
-            if (error) {
-                console.error(error);
-                return;
-            }
-
-            console.log(this.parseInsert(options))
-            this.connection.query(this.parseInsert(options));
-        });
-    }
-
-    public parseInsert(options: InsertQuery): string {
-        const config: InsertQuery = utils.parseConfig({
-            table: "",
-            data: [
-                {
-                    column: "",
-                    value: ""
-                }
-            ],
-            restrict: this.restrictDefaults
-        } as InsertQuery, options);
-
-        let allColumns: string[] = [];
-        let allValues: string[] = [];
-
-        config.data.forEach((columnGroup: any) => {
-            allColumns.push(columnGroup.column);
-            allValues.push(columnGroup.value);
-        });
-
-        let values: any[] = [ config.table, ...allColumns, ...allValues ];
-
-        const columnValuePlaceholders = this.parsePlaceholders(allColumns, "?");
-        const columnColumnPlaceholders = this.parsePlaceholders(allColumns);
-
-        let sqlQuery = this.addRestrict(`INSERT INTO ?? (${columnColumnPlaceholders}) VALUES (${columnValuePlaceholders})`, <QueryRestrict>config.restrict, values);
-        const finalQuery = mySql.format(sqlQuery, values, true);
-
-        return finalQuery;
-    }
-
-    public select(options: SelectQuery, callback: (result: any[]) => void) {
-        this.isReady((error: any) => {
-            if (error) {
-                console.error(error);
-                return;
-            }
-
-            this.connection.query(this.parseSelect(options), (error: any, result: any) => {
-                if (!error) {
-                    callback(this.cleanResult(result));
-                }
-            }); 
-        });
-    }
-
-    public parseSelect(options: SelectQuery): string {
-        const config: SelectQuery = utils.parseConfig({
-            table: "",
-            columns: "*",
-            restrict: this.restrictDefaults
-        } as SelectQuery, options);
-
-        const values: any[] = [  ];
-        let columnSingle = "";
-        if (Array.isArray(config.columns)) {
-            values.push(...config.columns);
-        } else {
-            columnSingle = config.columns;
-        }
-
-        values.push(config.table);
-        let renderColumns = "";
-
-        if (Array.isArray(config.columns) && config.columns.length > 1) {
-            (() => {
-                renderColumns = this.parsePlaceholders(config.columns);
-            })();
-        } else {
-            if (<string>config.columns == "*") {
-                renderColumns = "*";
-            } else {
-                renderColumns = mySql.escape(<string>config.columns);
-            }
-        }
-
-        const sqlQuery = this.addRestrict(`SELECT ${renderColumns} FROM ??`, config.restrict ? config.restrict : {}, values);
-        const finalQuery = mySql.format(sqlQuery, values);
-
-        return finalQuery;
-    }
-
-    public addRestrict(sqlQuery: string, options: QueryRestrict, values: any[]): string {
-        let query = sqlQuery;
-
-        if (options.limit !== null) {
-            if (typeof options.limit == "number") {
-                query += ` LIMIT ${options.limit}`;
-            } else {
-                query += " LIMIT ??";
-                values.push(options.limit);
-            }
-        }
-
-        return query;
-    }
-
-    private isReady(callback: (error: any) => void) {
+    public run(query: string, values: string[] | number[], callback: (error: any, results: any[] | null, metaData: MetaData | null) => void = () => {}) {
         if (this.ready) {
-            callback(null);
-            return;
+            this.connection.query(query, values, (error: any, results: any, fields: any) => {
+                if (error) {
+                    callback(error, null, null)
+                    return;
+                }
+
+                const result = [] as any[];
+
+                if (Array.isArray(results)) {
+                    results.forEach((resultSingle: any) => {
+                        result.push({ ...resultSingle })
+                    })
+
+                    callback(null, result, fields)
+                    return
+                }
+
+                callback(null, null, null)
+            })
+
+            callback(null, null, null)
+            return
         }
 
-        callback(new Error("The database connection is not ready"));
+        callback(new Error("Database connection is not ready"), null, null)
     }
 }
--- test/test.ts
+++ test/test.ts
@@ -1,29 +1,28 @@
-import { sql, terminal } from "../src/Main";
+import { sql, terminal } from "../src/Main"
 
-terminal.animate("Connecting to database server");
+terminal.animate("Connecting to database server")
 
 const db = sql.create({
     port: 3306,
     host: "localhost",
     database: "axeri"
-});
+})
 
 db.on("open", () => {
-    terminal.endAnimation("Connected to database", "success");
+    terminal.endAnimation("Connected to database", "success")
 
-    db.query.insert({
-        table: "accounts",
-        data: [
-            {
-                column: "name",
-                value: "Test"
-            }
-        ]
-    });
-});
+    db.query.run("UPDATE ?? SET name = ?, email = ?", [ "accounts", "OWO", "Email" ], (error: any, results: any) => {
+        
+        if (results) {
+            console.log(results)
+        } else if (error) {
+            console.error(error)
+        }
+    })
+})
 
 db.on("error", (error: any) => {
-    terminal.endAnimation(error, "error");
-});
+    terminal.endAnimation(error, "error")
+})
 
-db.run();
+db.run()
