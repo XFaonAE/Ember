f335b636c151ab40907faecc38c9c43e470ad3f1
XfaonAE <xfaonae@gmail.com>
2021-09-02 23:04:36 -0400

    Chore: Updated template

--- src/gui/template/src/vue/shims-vue.d.ts
+++ src/gui/template/src/vue/shims-vue.d.ts
@@ -1,5 +1,10 @@
 declare module "*.vue" {
-    import { defineComponent } from "vue";
-    const component: ReturnType<typeof defineComponent>;
+    import type { DefineComponent } from "vue";
+    const component: DefineComponent<{}, {}, any>;
     export default component;
+}
+
+declare module "*.less" {
+    const resource: { [ key: string ]: string };
+    export = resource;
 }
